4e7a4dc1b3cb460eae8f604a20e35833215a22d7
Grover <grovertabu@gmail.com>
2019-04-22 15:32:42 -0400

    Tarea 6 Orientado a objetos

---
+++ Practica 6 Taboada/index.php
@@ -0,0 +1,19 @@
+<?php session_start();?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Documento sin t&iacute;tulo</title>
+</head>
+<body>
+	<center>
+		<b><p><a href="forminsertar.php">Insertar</a></p>
+			<p><a href="formeliminar.php">Eliminar</a></p>
+			<p><a href="formmostrar.php">Mostrar</a> </p>
+			<p><a href="MostrarArmario.php">Mostrar Armario</a> </p></b>
+		<hr>
+	</center>
+	<h2>Nombre: Grover Taboada Rodas</h2>
+	<h2>Carreras: Ing Sistemas || Ing Cienccias de la computacion</h2>
+</body>
+</html>
